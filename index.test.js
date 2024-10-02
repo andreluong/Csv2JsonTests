ab14c9e6ef6d76a9ced12c0228729375c0a1e587
Brian <le.brian125@gmail.com>
2024-10-02 10:52:24 -0700

    wrapping json parse over file buffers

--- index.test.js
+++ index.test.js
@@ -1,19 +1,36 @@
 var csv2json = require('csv2json');
 var fs = require('fs');
+var diff = require('diff')
 
-function convert(inputFile, outputFile) {
-    fs.createReadStream(inputFile)
-        .pipe(csv2json())
-        .pipe(fs.createWriteStream(outputFile));
+function convert(inputFile, outputFile, dynamicTyping=false, separator=':') {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(inputFile)
+            .pipe(csv2json({ dynamicTyping, separator }))
+            .pipe(fs.createWriteStream(outputFile))
+            .on('finish', () => {
+                resolve();
+            })
+            .on('error', (error) => {
+                reject(error);
+            });
+    });
 }
 
-test('convert', async () => {
-    var inputFile = 'input/data.csv';
-    var outputFile = 'output/data.json';
-    var expectedOutputFile = 'expected_output/data.json';
 
-    convert(inputFile, outputFile);
-    
-    expect(fs.readFileSync(expectedOutputFile))
-        .toEqual(fs.readFileSync(outputFile));
+test('11: Fields containing quotation marks: dynamicTyped, separator=comma', async () => {
+    var inputFile = './input/11.csv';
+    var outputFile = './output/21.json';
+    var expectedOutputFile = './expected_output/21.json';
+
+    await convert(inputFile, outputFile, true, ',')
+    expect(JSON.parse(fs.readFileSync(expectedOutputFile))).toEqual(JSON.parse(fs.readFileSync(outputFile)))
+});
+
+test('11: Fields containing quotation marks: dynamicTyp=false, separator=abs', async () => {
+    var inputFile = './input/11.csv';
+    var outputFile = './output/21.json';
+    var expectedOutputFile = './expected_output/21.json';
+
+    await convert(inputFile, outputFile, true, ',')
+    expect(JSON.parse(fs.readFileSync(expectedOutputFile))).toEqual(JSON.parse(fs.readFileSync(outputFile)))
 });
