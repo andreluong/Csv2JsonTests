76ecfaa712b7cc3cc8c711ffcda8d0c4ffca940f
Brian <le.brian125@gmail.com>
2024-10-02 19:25:19 -0700

    adding additional input files

--- index.test.js
+++ index.test.js
@@ -22,44 +22,57 @@ beforeAll(() => {
 })
 
 
-// describe('1: Empty File', () => {
-//     test('A: dynamic_typing = true; separator = abs; tab_separators = false', () => {
+describe('1: Empty File', () => {
+    test('A: dynamic_typing = true; separator = abs', async () => {
+        const inputFile = 'input/1a.csv'
+        const outputFile = 'output/1a.json'
+        const expectedOutputFile = 'expected_output/1a.json'
 
-//     });
-//     test('B: dynamic_typing = false; separator = comma; tab_separators = true', () => {
+        await convert(inputFile, outputFile, true, '.');
 
-//     });
-// });
+        expect(JSON.parse(fs.readFileSync(expectedOutputFile))).toEqual(JSON.parse(fs.readFileSync(outputFile)));
 
-// describe('2: Empty header and one record and no characteristics listed in special character constraints', () => {
-//     test('A: dynamic_typing = true; separator = comma; tab_separators = false', async () => {
-//         const inputFile = 'input/2.csv';
-//         const outputFile = 'output/2.json';
-//         const expectedOutputFile = 'expected_output/2.json';
+    });
+    test('B: dynamic_typing = false; separator = comma; tab_separators = true',async  () => {
+        const inputFile = 'input/1b.csv'
+        const outputFile = 'output/1b.json'
+        const expectedOutputFile = 'expected_output/1b.json'
 
-//         await convert(inputFile, outputFile);
+        await convert(inputFile, outputFile, false, ',');
 
-//         expect(JSON.parse(fs.readFileSync(expectedOutputFile))).toEqual(JSON.parse(fs.readFileSync(outputFile)));
-//     });
-//     test('B: dynamic_typing = false; separator = abs; tab_separators = true', () => {
+        expect(JSON.parse(fs.readFileSync(expectedOutputFile))).toEqual(JSON.parse(fs.readFileSync(outputFile)));
+    });
+});
+
+describe('2: Empty header and one record and no characteristics listed in special character constraints', () => {
+    test('A: dynamic_typing = true; separator = comma; tab_separators = false', async () => {
+        const inputFile = 'input/2a.csv';
+        const outputFile = 'output/2a.json';
+        const expectedOutputFile = 'expected_output/2a.json';
+
+        await convert(inputFile, outputFile);
+
+        expect(JSON.parse(fs.readFileSync(expectedOutputFile))).toEqual(JSON.parse(fs.readFileSync(outputFile)));
+    });
+    test('B: dynamic_typing = false; separator = abs; tab_separators = true', () => {
         
-//     });
-// });
+    });
+});
 
-// describe('3: Empty header and many records and no characteristics listed in special character constraints', () => {
-//     test('A: dynamic_typing = true; separator = comma; tab_separators = false', () => {
-//         const inputFile = 'input/3.csv';
-//         const outputFile = 'output/3.json';
-//         const expectedOutputFile = 'expected_output/3.json';
+describe('3: Empty header and many records and no characteristics listed in special character constraints', () => {
+    test('A: dynamic_typing = true; separator = comma; tab_separators = false', () => {
+        const inputFile = 'input/3a.csv';
+        const outputFile = 'output/3a.json';
+        const expectedOutputFile = 'expected_output/3.json';
 
-//         convert(inputFile, outputFile, true);
+        convert(inputFile, outputFile, true);
 
-//         expect(JSON.parse(fs.readFileSync(expectedOutputFile))).toEqual(JSON.parse(fs.readFileSync(outputFile)));
-//     });
-//     test('B: dynamic_typing = false; separator = abs; tab_separators = true', () => {
+        expect(JSON.parse(fs.readFileSync(expectedOutputFile))).toEqual(JSON.parse(fs.readFileSync(outputFile)));
+    });
+    test('B: dynamic_typing = false; separator = abs; tab_separators = true', () => {
         
-//     });
-// });
+    });
+});
 
 describe('10 fields with newlines (not newline characters) that\’s not valid json objects', () => {
     test('A: dynamic_typing = true; separator = comma;', async () => {
@@ -73,24 +86,31 @@ describe('10 fields with newlines (not newline characters) that\’s not valid j
         const output = JSON.parse(fs.readFileSync(outputFile));
         expect(input).toEqual(output);
     });
-    test('B: dynamic_typing = false; separator = abs;', async () => {
-        const inputFile = 'input/10B.csv';
-        const outputFile = 'output/10B.json';
-        const expectedOutputFile = 'expected_output/10.json';
+    test('B: dynamic_typing = false; separator = comma', async () => {
+        const inputFile = 'input/1b.csv'
+        const outputFile = 'output/1b.json'
+        const expectedOutputFile = 'expected_output/1a.json'
 
-        await convert(inputFile, outputFile, false, "*");
+        await convert(inputFile, outputFile, false, ',');
 
-        const input = JSON.parse(fs.readFileSync(expectedOutputFile));
-        const output = JSON.parse(fs.readFileSync(outputFile));
-        expect(input).toEqual(output);
+        expect(JSON.parse(fs.readFileSync(expectedOutputFile))).toEqual(JSON.parse(fs.readFileSync(outputFile)));
     });
 });
 
-describe('11: fields with text containing quotation marks that\’s not valid json objects', () => {
-    test('A: dynamic_typing = true; separator = comma;', async () => {
-        const inputFile = 'input/11A.csv';
-        const outputFile = 'output/11A.json';
-        const expectedOutputFile = 'expected_output/11.json';
+describe('2: Empty header and one record and no characteristics listed in special character constraints', () => {
+    test('A: dynamic_typing = true; separator = comma', async () => {
+        const inputFile = 'input/2a.csv';
+        const outputFile = 'output/2a.json';
+        const expectedOutputFile = 'expected_output/2a.json';
+
+        await convert(inputFile, outputFile);
+
+        expect(JSON.parse(fs.readFileSync(expectedOutputFile))).toEqual(JSON.parse(fs.readFileSync(outputFile)));
+    });
+    test('B: dynamic_typing = false; separator = abs;', async () => {
+        const inputFile = 'input/2b.csv';
+        const outputFile = 'output/2b.json';
+        const expectedOutputFile = 'expected_output/2b.json';
 
         await convert(inputFile, outputFile, true);
 
@@ -98,24 +118,35 @@ describe('11: fields with text containing quotation marks that\’s not valid js
         const output = JSON.parse(fs.readFileSync(outputFile));
         expect(input).toEqual(output);
     });
-    test('B: dynamic_typing = false; separator = abs;', async () => {
-        const inputFile = 'input/11B.csv';
-        const outputFile = 'output/11B.json';
-        const expectedOutputFile = 'expected_output/11.json';
+});
 
-        await convert(inputFile, outputFile, false, "-");
+describe('3: Empty header and many records and no characteristics listed in special character constraints', () => {
+    test('A: dynamic_typing = true; separator = comma', async () => {
+        const inputFile = 'input/3a.csv';
+        const outputFile = 'output/3a.json';
+        const expectedOutputFile = 'expected_output/3a.json';
 
-        const input = JSON.parse(fs.readFileSync(expectedOutputFile));
-        const output = JSON.parse(fs.readFileSync(outputFile));
-        expect(input).toEqual(output);
+        await convert(inputFile, outputFile, true);
+
+        expect(JSON.parse(fs.readFileSync(expectedOutputFile))).toEqual(JSON.parse(fs.readFileSync(outputFile)));
+    });
+    test('B: dynamic_typing = false; separator = abs', async () => {
+        const inputFile = 'input/3b.csv';
+        const outputFile = 'output/3b.json';
+        const expectedOutputFile = 'expected_output/3b.json';
+
+        await convert(inputFile, outputFile, false, ';');
+
+        expect(JSON.parse(fs.readFileSync(expectedOutputFile))).toEqual(JSON.parse(fs.readFileSync(outputFile)));
     });
 });
 
-describe('12: fields with text containing commas that\’s not valid json objects', () => {
-    test('A: dynamic_typing = true; separator = comma;', async () => {
-        const inputFile = 'input/12A.csv';
-        const outputFile = 'output/12A.json';
-        const expectedOutputFile = 'expected_output/12.json';
+
+describe('3: Empty header and many records and no characteristics listed in special character constraints', () => {
+    test('A: dynamic_typing = true; separator = comma', async () => {
+        const inputFile = 'input/3a.csv';
+        const outputFile = 'output/3a.json';
+        const expectedOutputFile = 'expected_output/3a.json';
 
         await convert(inputFile, outputFile, true);
 
@@ -123,40 +154,13 @@ describe('12: fields with text containing commas that\’s not valid json object
         const output = JSON.parse(fs.readFileSync(outputFile));
         expect(input).toEqual(output);
     });
-    test('B: dynamic_typing = false; separator = abs;', async () => {
-        const inputFile = 'input/12B.csv';
-        const outputFile = 'output/12B.json';
-        const expectedOutputFile = 'expected_output/12.json';
+    test('B: dynamic_typing = false; separator = abs', async () => {
+        const inputFile = 'input/3b.csv';
+        const outputFile = 'output/3b.json';
+        const expectedOutputFile = 'expected_output/3b.json';
 
-        await convert(inputFile, outputFile, false, ";");
+        await convert(inputFile, outputFile, false, ';');
 
-        const input = JSON.parse(fs.readFileSync(expectedOutputFile));
-        const output = JSON.parse(fs.readFileSync(outputFile));
-        expect(input).toEqual(output);
-    });
-});
-
-describe('13: fields with characters that can\’t normally be typed on a keyboard (e.g. control characters)', () => {
-    test('A: dynamic_typing = true; separator = comma;', async () => {
-        const inputFile = 'input/13A.csv';
-        const outputFile = 'output/13A.json';
-        const expectedOutputFile = 'expected_output/13.json';
-
-        await convert(inputFile, outputFile, true);
-
-        const input = JSON.parse(fs.readFileSync(expectedOutputFile));
-        const output = JSON.parse(fs.readFileSync(outputFile));
-        expect(input).toEqual(output);
-    });
-    test('B: dynamic_typing = false; separator = abs;', async () => {
-        const inputFile = 'input/13B.csv';
-        const outputFile = 'output/13B.json';
-        const expectedOutputFile = 'expected_output/13.json';
-
-        await convert(inputFile, outputFile, false, ':');
-
-        const input = JSON.parse(fs.readFileSync(expectedOutputFile));
-        const output = JSON.parse(fs.readFileSync(outputFile));
-        expect(input).toEqual(output);
+        expect(JSON.parse(fs.readFileSync(expectedOutputFile))).toEqual(JSON.parse(fs.readFileSync(outputFile)));
     });
 });
